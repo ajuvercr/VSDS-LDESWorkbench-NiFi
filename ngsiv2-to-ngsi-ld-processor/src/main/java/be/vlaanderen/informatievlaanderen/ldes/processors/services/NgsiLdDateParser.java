308c3b9e36b3dd1bd7cce3a499e7dcafef3456f7
Stijn Verholen <boro@inuits.eu>
2022-10-06 21:24:36 +0200

    fix: VSDSPUB-309 split workbench to proper repo

---
+++ ngsiv2-to-ngsi-ld-processor/src/main/java/be/vlaanderen/informatievlaanderen/ldes/processors/services/NgsiLdDateParser.java
@@ -0,0 +1,15 @@
+package be.vlaanderen.informatievlaanderen.ldes.processors.services;
+
+public class NgsiLdDateParser {
+
+	private NgsiLdDateParser() {
+	}
+
+	public static String normaliseDate(String date) {
+		if (!date.endsWith("Z")) {
+			return date + "Z";
+		}
+
+		return date;
+	}
+}
