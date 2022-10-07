e3f1058f152360697a067174881b337704f94a02
sverholen <sverholen@gmail.com>
2022-10-07 12:01:36 +0200

    fix: VSDSPUB-309 split workbench to proper repo (#1)
    * fix: VSDSPUB-309 split workbench to proper repo
    
    * fix/VSDSPUB-309: minor changes + workflows
    
    Co-authored-by: Stijn Verholen <boro@inuits.eu>

---
+++ ngsi-ld-to-ldes-processor/src/main/java/be/vlaanderen/informatievlaanderen/ldes/processors/services/OutputFormatConverter.java
@@ -0,0 +1,51 @@
+package be.vlaanderen.informatievlaanderen.ldes.processors.services;
+
+import org.apache.jena.datatypes.TypeMapper;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.riot.Lang;
+
+import be.vlaanderen.informatievlaanderen.ldes.client.converters.ModelConverter;
+import be.vlaanderen.informatievlaanderen.ldes.processors.valueobjects.MemberInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.jena.rdf.model.ResourceFactory.*;
+
+public class OutputFormatConverter {
+
+	private static final String PROV_GENERATED_AT_TIME = "http://www.w3.org/ns/prov#generatedAtTime";
+	private static final String XMLSCHEMA_DATE_TIME = "https://www.w3.org/2001/XMLSchema#DateTime";
+
+	private final Lang outputFormat;
+	private final boolean addTopLevelGeneratedAt;
+
+	public OutputFormatConverter(Lang outputFormat, boolean addTopLevelGeneratedAt) {
+		this.outputFormat = outputFormat;
+		this.addTopLevelGeneratedAt = addTopLevelGeneratedAt;
+	}
+
+	public String convertToDesiredOutputFormat(String jsonInput, MemberInfo memberInfo) {
+		Model model = ModelConverter.convertStringToModel(jsonInput, Lang.JSONLD11);
+		addAdditionalStatements(memberInfo, model);
+		return ModelConverter.convertModelToString(model, outputFormat);
+
+	}
+
+	public Lang getOutputFormat() {
+		return outputFormat;
+	}
+
+	private void addAdditionalStatements(MemberInfo memberInfo, Model model) {
+		Resource resource = model.listSubjects().filterKeep(subject -> !subject.isAnon()).nextOptional()
+				.orElseThrow(RuntimeException::new);
+		List<Statement> statements = new ArrayList<>();
+		if (addTopLevelGeneratedAt)
+			statements.add(createStatement(resource, createProperty(PROV_GENERATED_AT_TIME),
+					createTypedLiteral(memberInfo.getObservedAt(),
+							TypeMapper.getInstance().getTypeByName(XMLSCHEMA_DATE_TIME))));
+		model.add(statements);
+	}
+}
